9f5c8603237744b0c83dae507171b73007890893
Meelis Pihlap <meelispihlap@gmail.com>
2022-07-27 15:31:16 +0300

    Fixed logging

--- mod_updater.py
+++ mod_updater.py
@@ -28,41 +28,41 @@ def move_contents_here(folder):
 
 
 def download_file(url, target_filename=None, zip=False):
-    try:
-        if url.startswith("https://drive.google.com/"):
-            if "folder" in url:
-                gdown.download_folder(url, quiet=False, output="temp_folder")
-                move_contents_here("temp_folder")
-                rmdir("temp_folder")
-            else:
-                if zip:
-                    gdown.download(url, output="temp_zip.zip", quiet=False, fuzzy=True)
-                    with zipfile.ZipFile("temp_zip.zip", 'r') as zip_ref:
-                        members = zip_ref.namelist()
-                        logging.debug(f"members in zip {members}")
-                        zip_ref.extractall()
-                    remove("temp_zip.zip")
-                    if members[0][-1] == "/": # If files are nested in a folder, get them out
-                        move_contents_here(members[0])
-                        rmdir(members[0])
-                else:
-                    gdown.download(url, quiet=False, output=target_filename, fuzzy=True)
+    #try:
+    if url.startswith("https://drive.google.com/"):
+        if "folder" in url:
+            gdown.download_folder(url, quiet=False, output="temp_folder")
+            move_contents_here("temp_folder")
+            rmdir("temp_folder")
         else:
-            get_response = requests.get(url,stream=True)
-            file_name = url.split("/")[-1] if target_filename is None else target_filename
-            with open(file_name, 'wb') as f:
-                for chunk in get_response.iter_content(chunk_size=1024):
-                    if chunk: # filter out keep-alive new chunks
-                        f.write(chunk)
             if zip:
-                with zipfile.ZipFile(file_name, 'r') as zip_ref:
+                gdown.download(url, output="temp_zip.zip", quiet=False, fuzzy=True)
+                with zipfile.ZipFile("temp_zip.zip", 'r') as zip_ref:
+                    members = zip_ref.namelist()
+                    logging.debug(f"members in zip {members}")
                     zip_ref.extractall()
-                remove(file_name)
-        return True
-    except Exception as e:
-        logging.warning(e)
-        logging.warning(f"Downloading url {url} unsuccessful.")
-        return False
+                remove("temp_zip.zip")
+                if members[0][-1] == "/": # If files are nested in a folder, get them out
+                    move_contents_here(members[0])
+                    rmdir(members[0])
+            else:
+                gdown.download(url, quiet=False, output=target_filename, fuzzy=True)
+    else:
+        get_response = requests.get(url,stream=True)
+        file_name = url.split("/")[-1] if target_filename is None else target_filename
+        with open(file_name, 'wb') as f:
+            for chunk in get_response.iter_content(chunk_size=1024):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+        if zip:
+            with zipfile.ZipFile(file_name, 'r') as zip_ref:
+                zip_ref.extractall()
+            remove(file_name)
+    return True
+    #except Exception as e:
+    #    logging.warning(e)
+    #    logging.warning(f"Downloading url {url} unsuccessful.")
+    #    return False
 
 
 class Updater(ABC):
@@ -282,6 +282,13 @@ def get_metafile(executable, metafile):
     return metafile_path
 
 
+def set_loglevel(loglevel):
+    if loglevel == "DEBUG":
+        logging.basicConfig(level=logging.DEBUG)
+    elif loglevel == "INFO":
+        logging.basicConfig(level=logging.INFO)
+
+
 def main():
 
     if getattr(sys, 'frozen', False):
@@ -290,26 +297,23 @@ def main():
         start_dir = dirname(os.path.abspath(__file__))
     chdir(start_dir) # Move to defed bin folder, if not executing script from there
     if not start_dir.endswith('Divinity Original Sin 2\DefEd\\bin'):
-        logging.warning(f"Your current directory {start_dir} does not seem to be correct. The path should end with 'Divinity Original Sin 2\DefEd\\bin'")
-        logging.warning(f"Press ENTER to continue anyway, Ctrl+C to cancel.")
+        print(f"Your current directory {start_dir} does not seem to be correct. The path should end with 'Divinity Original Sin 2\DefEd\\bin'")
+        print(f"Press ENTER to continue anyway, Ctrl+C to cancel.")
         input()
 
     with open("mod_updater_config.yaml", "r") as stream:
         try:
-            params = yaml.safe_load(stream)
+            params = yaml.safe_load(stream) # Throws yaml parse error
         except yaml.YAMLError as exc:
-            logging.error("Unable to parse yaml. Check your syntax.")
-            logging.error(exc)
-            exit(1)
+            raise yaml.YAMLError("Unable to parse yaml. Check your syntax.")
 
-    logging.debug(f"yaml contents {params}")
     global_settings = params["Global"]
     force_update_all = global_settings["force_update_all"]
     loglevel = global_settings["loglevel"]
-    if loglevel == "DEBUG":
-        logging.basicConfig(level=logging.DEBUG)
-    elif loglevel == "INFO":
-        logging.basicConfig(level=logging.INFO)
+    set_loglevel(loglevel)
+
+    logging.debug(f"yaml contents {params}")
+
 
     executable = global_settings["executable"]
     mod_folder = global_settings["mod_folder"]
